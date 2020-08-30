35fa812e49a6a3774f6b88a045e54eb551411a75
Germey <cqc@cuiqingcai.com>
2020-08-31 03:22:16 +0800

    init

---
+++ spidermiddlewaredemo/middlewares.py
@@ -0,0 +1,38 @@
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals, Item
+
+# useful for handling different item types with a single interface
+from itemadapter import is_item, ItemAdapter
+
+from spidermiddlewaredemo.items import DemoItem
+
+
+class CustomizeMiddleware(object):
+    
+    def process_spider_input(self, response, spider):
+        response.status = 201
+        return None
+    
+    def process_spider_output(self, response, result, spider):
+        for i in result:
+            if isinstance(i, DemoItem):
+                i['origin'] = None
+                yield i
+    
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+        
+        # Should return either None or an iterable of Request or item objects.
+        pass
+    
+    def process_start_requests(self, start_requests, spider):
+        for request in start_requests:
+            url = request.url
+            url += '&name=germey'
+            request = request.replace(url=url)
+            yield request
---
+++ spidermiddlewaredemo/spiders/httpbin.py
@@ -0,0 +1,19 @@
+from scrapy import Spider, Request
+
+from spidermiddlewaredemo.items import DemoItem
+
+
+class HttpbinSpider(Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    start_url = 'http://httpbin.org/get'
+    
+    def start_requests(self):
+        for i in range(5):
+            url = f'{self.start_url}?query={i}'
+            yield Request(url, callback=self.parse)
+    
+    def parse(self, response):
+        print('Status', response.status)
+        item = DemoItem(**response.json())
+        yield item
